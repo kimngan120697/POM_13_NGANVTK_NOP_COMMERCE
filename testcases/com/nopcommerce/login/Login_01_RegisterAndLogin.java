355006e228bf8166234841e075634a84aff4074c
vn5b <vn5b@172.20.16.25>
2020-10-09 17:22:57 +0700

    update

--- testcases/com/nopcommerce/login/Login_01_RegisterAndLogin.java
+++ testcases/com/nopcommerce/login/Login_01_RegisterAndLogin.java
@@ -35,67 +35,45 @@ public class Login_01_RegisterAndLogin {
 	public void TC_01_RegisterToSystem() {
 
 		// Click to Register link
-		driver.findElement(By.cssSelector(".ico-register")).click();
+		
 
 		// Click mail radio button
-		driver.findElement(By.cssSelector("#gender-male")).click();
 
 		// Input to firstname textbox
-		driver.findElement(By.cssSelector("#FirstName")).sendKeys("Corona");
 
 		// Input to lasttname textbox
-		driver.findElement(By.cssSelector("#LastName")).sendKeys("Virus");
 
 		// Select item in Day
-		select = new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthDay']")));
-		select.selectByVisibleText("1");
 		// Select item in Month
-		select = new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthMonth']")));
-		select.selectByVisibleText("February");
 
 		// Select item in Year
-		select = new Select(driver.findElement(By.cssSelector("select[name='DateOfBirthYear']")));
-		select.selectByVisibleText("1997");
 
 		// Input to email text box
-		driver.findElement(By.cssSelector("#Email")).sendKeys(email);
 
 		// Input to company text box
-		driver.findElement(By.cssSelector("#Company")).sendKeys("CoronaCompany");
 
 		// Input to password text box
-		driver.findElement(By.cssSelector("#Password")).sendKeys(password);
 
 		// Input to confirm password text box
-		driver.findElement(By.cssSelector("#ConfirmPassword")).sendKeys(password);
 
 		// Input to firstname textbox
-		driver.findElement(By.cssSelector("#register-button")).click();
 
 		// Verify register success
-		String resultText = driver.findElement(By.cssSelector(".result")).getText();
-		Assert.assertEquals(resultText, "Your registration completed");
 
 		// Click to Logout link
-		driver.findElement(By.cssSelector(".ico-logout")).click();
 	}
 
 	@Test
 	public void TC_02_LoginToSystem() {
 		// Click to Login link
-		driver.findElement(By.cssSelector(".ico-login")).click();
 		
 		// Input to email text box
-		driver.findElement(By.cssSelector("#Email")).sendKeys(email);
 		
 		// Input to password text box
-		driver.findElement(By.cssSelector("#Password")).sendKeys(password);
 		
 		// Input to Login Button
-		driver.findElement(By.cssSelector(".login-button")).click();
 		
 		//Verify My Account Link displayed
-		Assert.assertTrue(driver.findElement(By.cssSelector(".ico-account")).isDisplayed());
 		
 
 	}
